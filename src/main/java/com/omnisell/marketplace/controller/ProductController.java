2f4229e820aafebe37197958f652bcc4b1438c32
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-06-17 19:08:20 -0300

    feat: CRUD completo de Product com testes no Postman

---
+++ src/main/java/com/omnisell/marketplace/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.omnisell.marketplace.controller;
+
+import com.omnisell.marketplace.model.Product;
+import com.omnisell.marketplace.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+}
